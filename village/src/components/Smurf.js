031461e9c916513fa641dd766eabe5ff128fffdd
ParrishJ <jarparrish@gmail.com>
2019-05-10 10:47:14 -0700

    Delete functionality added

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
-};
+class Smurf extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  deleteSmurfTwo = e => {
+    e.preventDefault();
+    this.props.deleteSmurf(this.props.id);
+  };
+
+  render() {
+    return (
+      <div className="Smurf">
+        <h3>{this.props.name}</h3>
+        <strong>{this.props.height} tall</strong>
+        <p>{this.props.age} smurf years old</p>
+        <button onClick={this.deleteSmurfTwo}>Delete Smurf</button>
+      </div>
+    );
+  }
+}
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
