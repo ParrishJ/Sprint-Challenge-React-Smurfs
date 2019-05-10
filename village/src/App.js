e5f466604a76ac0a0c9c8278f7a01dd47218c035
ParrishJ <jarparrish@gmail.com>
2019-05-10 08:14:29 -0700

    Dependencies added, ready to start project

--- village/src/App.js
+++ village/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  // You'll need to make sure you have the right properties on state and pass them down to props. All depenencies added!
   render() {
     return (
       <div className="App">
